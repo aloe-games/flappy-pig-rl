925f62ed39407617b35ba422406991abb4693184
Marek Michalik <marekmichalik43@gmail.com>
2024-06-01 23:41:18 +0200

    Clean up

--- agent.cpp
+++ agent.cpp
@@ -4,12 +4,7 @@
 
 using namespace std;
 
-const double Agent::EPS_START = 1.0;
-const double Agent::EPS_DECAY = 0.995;
-const double Agent::EPS_END = 0.01;
-
 const double Agent::GAMMA = 0.99;
-
 const double Agent::ALPHA = 0.0001;
 
 double uniform() {
@@ -27,40 +22,21 @@ double relu(double x) {
     return maximum(x, 0);
 }
 
-Agent::Agent() {
-    W[0][0] = uniform();
-    W[0][1] = uniform();
-    W[1][0] = uniform();
-    W[1][1] = uniform();
-    b[0] = uniform();
-    b[1] = uniform();
-    step = 0;
-    eps = Agent::EPS_START;
+double Agent::forward(double observation[2], int action) {
+    return relu(observation[0] * W[action][0] + observation[1] * W[action][1] + b[action]);
 }
 
 bool Agent::act(double observation[2]) {
-    if (uniform() > eps) {
-        double a[2];
-        for (int i = 0; i < 2; i++) {
-            a[i] = relu(observation[0] * W[i][0] + observation[1] * W[i][1] + b[i]);
-        }
-        return int(a[1] > a[0]);
-    }
-    return rand() % 2;
+    return forward(observation, 1) > forward(observation, 0);
 }
 
 void Agent::learn(double observation[2], bool action, double reward, double next_observation[2], bool done) {
-    step++;
-
-    if (step % 5 == 0) {
-        eps = max(eps * Agent::EPS_DECAY, Agent::EPS_END);
-    }
-
     double A, Y, dA, dZ, dW[2], db;
-    A = relu(observation[0] * W[action][0] + observation[1] * W[action][1] + b[action]);
+
+    A = forward(observation, action);
     Y = reward + (1 - int(done)) * Agent::GAMMA * maximum(
-        relu(next_observation[0] * W[0][0] + next_observation[1] * W[0][1] + b[0]),
-        relu(next_observation[0] * W[1][0] + next_observation[1] * W[1][1] + b[1])
+        forward(next_observation, 0),
+        forward(next_observation, 1)
     );
 
     dA = 2 * (A - Y);
@@ -72,8 +48,4 @@ void Agent::learn(double observation[2], bool action, double reward, double next
     W[action][0] -= Agent::ALPHA * dW[0];
     W[action][1] -= Agent::ALPHA * dW[1];
     b[action] -= Agent::ALPHA * db;
-
-    if (done) {
-        step = 0;
-    }
 }
--- agent.hpp
+++ agent.hpp
@@ -1,26 +1,21 @@
 #ifndef _agent_h
 #define _agent_h
 
-class Agent {
-    public:
-        static const double EPS_START;
-        static const double EPS_DECAY;
-        static const double EPS_END;
-        static const double GAMMA;
-        static const double ALPHA;
-    protected:
-        double W[2][2];
-        double b[2];
-        int step;
-        double eps;
-    public:
-        Agent();
-        bool act(double observation[2]);
-        void learn(double observation[2], bool action, double reward, double next_observation[2], bool done);
-};
-
 double uniform();
 double maximum(double a, double b);
 double relu(double x);
+
+class Agent {
+    public:
+        static const double GAMMA;
+        static const double ALPHA;
+    protected:
+        double W[2][2] = {{uniform(), uniform()}, {uniform(), uniform()}};
+        double b[2] = {uniform(), uniform()};
+    public:
+        double forward(double observation[2], int action);
+        bool act(double observation[2]);
+        void learn(double observation[2], bool action, double reward, double next_observation[2], bool done);
+};
 
 #endif
